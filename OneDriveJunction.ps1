63bf16378995719786bc58111e109d6cd5ea47b3
Eric Nemchik <eric@nemchik.com>
2017-11-19 09:55:23 -0600

    Check if the folder exists, and silently continue on error

--- OneDriveJunction.ps1
+++ OneDriveJunction.ps1
@@ -8,16 +8,18 @@ $junctionFolders = @(
   "Music",
   "Pictures",
   "Saved Games",
+  "Searches",
+  "fake",
   "Videos"
 );
 
 foreach ($folder in $junctionFolders) {
   $originalPath = "~\$folder\";
   $oneDrivePath = "~\OneDrive\$folder\";
-  if ((Get-Item $originalPath).Attributes.ToString().Contains("ReparsePoint") -ne $True) {
-    New-Item $oneDrivePath -Type Directory -Force;
-    Move-Item -Path "$($originalPath)*" -Destination $oneDrivePath -Force;
-    Remove-Item $originalPath -Recurse -Force;
-    New-Item -ItemType Junction -Path $originalPath -Value $oneDrivePath -Force;
+  if (((Test-Path $originalPath) -ne $True) -or ((Get-Item $originalPath).Attributes.ToString().Contains("ReparsePoint") -ne $True)) {
+    New-Item $oneDrivePath -Type Directory -Force -ErrorAction SilentlyContinue;
+    Move-Item -Path "$($originalPath)*" -Destination $oneDrivePath -Force -ErrorAction SilentlyContinue;
+    Remove-Item $originalPath -Recurse -Force -ErrorAction SilentlyContinue;
+    New-Item -ItemType Junction -Path $originalPath -Value $oneDrivePath -Force -ErrorAction SilentlyContinue;
   }
 }
