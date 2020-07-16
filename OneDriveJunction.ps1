182016dacdbabdd2bab77df528ec8d7a39212041
Eric Nemchik <eric@nemchik.com>
2020-07-16 08:39:08 -0500

    Update OneDriveJunction.ps1

--- OneDriveJunction.ps1
+++ OneDriveJunction.ps1
@@ -18,7 +18,8 @@ foreach ($folder in $junctionFolders) {
   if (((Test-Path $originalPath) -ne $True) -or ((Get-Item $originalPath).Attributes.ToString().Contains("ReparsePoint") -ne $True)) {
     New-Item $oneDrivePath -Type Directory -Force -ErrorAction SilentlyContinue;
     Move-Item -Path "$($originalPath)*" -Destination $oneDrivePath -Force -ErrorAction SilentlyContinue;
-    Remove-Item $originalPath -Recurse -Force -ErrorAction SilentlyContinue;
+    # This next line is dangerous and was not well thought out.
+    #Remove-Item $originalPath -Recurse -Force -ErrorAction SilentlyContinue;
     New-Item -ItemType Junction -Path $originalPath -Value $oneDrivePath -Force -ErrorAction SilentlyContinue;
   }
 }
