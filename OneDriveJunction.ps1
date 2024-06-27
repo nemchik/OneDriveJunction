114d48d0c74dc688c63f1f2517c7a77fdb6b9c53
Eric Nemchik <eric@nemchik.com>
2024-06-26 22:10:14 -0500

    feat: :sparkles: use environment vars
    Signed-off-by: Eric Nemchik <eric@nemchik.com>

--- OneDriveJunction.ps1
+++ OneDriveJunction.ps1
@@ -8,13 +8,15 @@ $junctionFolders = @(
 );
 
 foreach ($folder in $junctionFolders) {
-  $originalPath = "~\$folder\";
-  $oneDrivePath = "~\OneDrive\$folder\";
+  $originalPath = (Get-Item "$env:USERPROFILE\$folder").FullName;
+  $oneDrivePath = (Get-Item "$env:OneDrive\$folder").FullName;
   if (((Test-Path $originalPath) -ne $True) -or ((Get-Item $originalPath).Attributes.ToString().Contains("ReparsePoint") -ne $True)) {
     New-Item $oneDrivePath -Type Directory -Force -ErrorAction SilentlyContinue;
-    Move-Item -Path "$($originalPath)*" -Destination $oneDrivePath -Force -ErrorAction SilentlyContinue;
-    # This next line is dangerous and was not well thought out.
-    #Remove-Item $originalPath -Recurse -Force -ErrorAction SilentlyContinue;
+    Move-Item -Path "$($originalPath)\*" -Destination $oneDrivePath -Force -ErrorAction SilentlyContinue;
+    ## Potentially find a way to check if the folder is in use before removing it.
+    # Get-Process | Where-Object { $_.Modules.FileName -icontains $originalPath } | Stop-Process;
+    ## Remove the original folder.
+    # Remove-Item $originalPath -Recurse -Force -ErrorAction SilentlyContinue;
     New-Item -ItemType Junction -Path $originalPath -Value $oneDrivePath -Force -ErrorAction SilentlyContinue;
   }
 }
